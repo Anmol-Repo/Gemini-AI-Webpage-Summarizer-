3160af42b41c08a3e78da1d365132907cd4e40c2
Anmol-Repo <148074536+Anmol-Repo@users.noreply.github.com>
2026-06-30 22:49:09 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-chrome.runtime.onInstalled.addListener(()=>{
+chrome.runtime.onInstalled.addListener(()=>{   
     chrome.storage.sync.get(["geminiApiKey"],(result)=>{  
         if(!result.geminiApiKey){ 
             chrome.tabs.create({url: "options.html"});
